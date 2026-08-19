4bd2a5ac5ac02825d144a20fd82e1c12302fc7cc
LynasTing <LynasTing@proton.me>
2026-08-19 14:48:32 +0800

    feat(api-docs): configure OpenAPI metadata

--- src/main/java/com/lynas/slashadmin/LynasSlashAdminServerApplication.java
+++ src/main/java/com/lynas/slashadmin/LynasSlashAdminServerApplication.java
@@ -1,12 +1,19 @@
 package com.lynas.slashadmin;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+  info = @Info(title = "Lynas Slash Admin API", version = "v1", description = "Lynas Slash Admin 后端接口文档"))
 public class LynasSlashAdminServerApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(LynasSlashAdminServerApplication.class, args);
+  public static void main(
+    String[] args) {
+    WebServerApplicationContext applicationContext = (WebServerApplicationContext) SpringApplication
+      .run(LynasSlashAdminServerApplication.class, args);
+    int port = applicationContext.getWebServer().getPort();
+    System.out.println("Swagger UI: http:" + "/" + "/localhost:" + port + "/swagger-ui/index.html");
   }
 }
