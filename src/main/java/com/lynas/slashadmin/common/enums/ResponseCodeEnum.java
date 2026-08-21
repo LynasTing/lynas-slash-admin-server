3e1206fe98f6148b8eff0c4bb09467bbc796282a
LynasTing <LynasTing@proton.me>
2026-08-21 09:10:58 +0800

    feat(common): extend response codes

--- src/main/java/com/lynas/slashadmin/common/enums/ResponseCodeEnum.java
+++ src/main/java/com/lynas/slashadmin/common/enums/ResponseCodeEnum.java
@@ -1,15 +1,23 @@
 package com.lynas.slashadmin.common.enums;
-
-public enum ResponseCodeEnum {
-  /** 成功 */
+public enum ResponseCodeEnum implements ResponseCode {
+  /**
+   * 成功
+   */
   SUCCESS(200, "操作成功"),
-
-  /** 鉴权失败（未登录或 token 过期） */
-  NEED_LOGIN(401, "需登录后操作");
-
+  /**
+   * 鉴权失败（未登录或 token 过期）
+   */
+  NEED_LOGIN(401, "需登录后操作"),
+  /**
+   * 异常错误
+   */
+  SYSTEM_ERROR(500, "异常错误"),
+  /**
+   * ID 为空
+   */
+  ID_IS_NULL(513, "ID 不能为空");
   private int code;
   private String message;
-
   ResponseCodeEnum(int code, String message) {
     this.code = code;
     this.message = message;
