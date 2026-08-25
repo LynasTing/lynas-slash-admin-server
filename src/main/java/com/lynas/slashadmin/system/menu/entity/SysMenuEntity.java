ed38c1be29c72e692793379f8e018358ca70d1c6
LynasTing <LynasTing@proton.me>
2026-08-25 09:26:12 +0800

    style(menu): apply entity spacing

--- src/main/java/com/lynas/slashadmin/system/menu/entity/SysMenuEntity.java
+++ src/main/java/com/lynas/slashadmin/system/menu/entity/SysMenuEntity.java
@@ -1,4 +1,5 @@
 package com.lynas.slashadmin.system.menu.entity;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -24,58 +25,72 @@ public class SysMenuEntity {
    */
   @TableId(type = IdType.AUTO)
   private Long id;
+
   /**
    * 父级菜单ID, 0表示根节点
    */
   private Object parentId;
+
   /**
    * 菜单名称
    */
   private String name;
+
   /**
    * 唯一编码
    */
   private String code;
+
   /**
    * 节点类型 1 分组 2 目录 3 菜单 4 操作按钮
    */
   private Object category;
+
   /**
    * 排序值，越小越靠前
    */
   private Object sort;
+
   /**
    * 状态 0 禁用  1 启用
    */
   private Object status;
+
   /**
    * 前端路由路径
    */
   private String path;
+
   /**
    * 前端组件标识或组件路径
    */
   private String component;
+
   /**
    * 菜单图标
    */
   private String icon;
+
   /**
    * 是否隐藏 0 否 1 是
    */
   private Object hidden;
+
   /**
    * 菜单描述
    */
   private String description;
+
   /**
    * 外链地址
    */
   private String externalLink;
+
   /**
    * 创建时间
    */
   private LocalDateTime createdAt;
+
   /**
    * 更新时间
    */
